eec62b7e412397c1c1e8428787be9ef62c48c910
ig-varshan <2024it100@stu.vau.ac.lk>
2026-06-16 18:27:20 +0530

    Add student information logging in exc1.js

---
+++ JAVA/EXCERCISE/exc1.js
@@ -0,0 +1,27 @@
+let name="Mohan";
+let age=20;
+let marks=90;
+let registered=true;
+
+console.log("Student name: "+name);
+console.log("Student age: "+age);
+console.log("Student marks: "+marks);
+console.log("IS Student registered? : "+registered);
+
+if (marks> 50 ){
+					console.log("Pass")
+					}
+					
+					else	{
+		console.log("Fail")
+			}	
+
+
+const subjects=["Maths","Science","English","ICT"]
+
+
+for(let key in subjects) {
+		console.log(subjects[key]);
+}			
+
+
